0ded0c56be49b669a7899b363fa2af708b8c6337
desertwitch <24509509+desertwitch@users.noreply.github.com>
2025-11-01 09:36:37 +0100

    raise timeout on debug package

--- source/nut-dw/usr/local/emhttp/plugins/nut-dw/include/nut_debug.php
+++ source/nut-dw/usr/local/emhttp/plugins/nut-dw/include/nut_debug.php
@@ -17,7 +17,7 @@
  * included in all copies or substantial portions of the Software.
  *
  */
-exec("timeout -s9 30 /usr/local/emhttp/plugins/nut-dw/scripts/debug", $debug_result);
+exec("timeout -s9 120 /usr/local/emhttp/plugins/nut-dw/scripts/debug", $debug_result);
 if(!empty($debug_result)) {
     if(strpos(end($debug_result), "DONE:") !== false) {
         $debugFile = explode(":", end($debug_result))[1];
@@ -32,7 +32,7 @@ if(!empty($debug_result)) {
         } else {
             echo("ERROR: The NUT Debug Package Generation Script has failed - bash backend returned filename, php could not find file.");
         }
-    } else { 
+    } else {
         echo("ERROR: The NUT Debug Package Generation Script has failed - response from the bash backend:<br><pre>");
         echo(implode("\n",$debug_result));
         echo("</pre>");
